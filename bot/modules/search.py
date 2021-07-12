d248761397e21a0b8707d03ff5f14a48f287e3ba
harshaxvarthan <82403923+harshaxvarthan@users.noreply.github.com>
2021-07-12 18:33:33 +0530

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -68,81 +68,8 @@ async def return_search(query, page=1, sukebei=False):
 message_info = dict()
 ignore = set()
 
-@app.on_message(filters.command(['nyaasi']))
-async def nyaa_search(client, message):
-    text = message.text.split(' ')
-    text.pop(0)
-    query = ' '.join(text)
-    await init_search(client, message, query, False)
 
-@app.on_message(filters.command(['sukebei']))
-async def nyaa_search_sukebei(client, message):
-    text = message.text.split(' ')
-    text.pop(0)
-    query = ' '.join(text)
-    await init_search(client, message, query, True)
 
-async def init_search(client, message, query, sukebei):
-    result, pages, ttl = await return_search(query, sukebei=sukebei)
-    if not result:
-        await message.reply_text('No results found')
-    else:
-        buttons = [InlineKeyboardButton(f'1/{pages}', 'nyaa_nop'), InlineKeyboardButton(f'Next', 'nyaa_next')]
-        if pages == 1:
-            buttons.pop()
-        reply = await message.reply_text(result, reply_markup=InlineKeyboardMarkup([
-            buttons 
-        ]))
-        message_info[(reply.chat.id, reply.message_id)] = message.from_user.id, ttl, query, 1, pages, sukebei
-
-@app.on_callback_query(custom_filters.callback_data('nyaa_nop'))
-async def nyaa_nop(client, callback_query):
-    await callback_query.answer(cache_time=3600)
-
-callback_lock = asyncio.Lock()
-@app.on_callback_query(custom_filters.callback_data(['nyaa_back', 'nyaa_next']))
-async def nyaa_callback(client, callback_query):
-    message = callback_query.message
-    message_identifier = (message.chat.id, message.message_id)
-    data = callback_query.data
-    async with callback_lock:
-        if message_identifier in ignore:
-            await callback_query.answer()
-            return
-        user_id, ttl, query, current_page, pages, sukebei = message_info.get(message_identifier, (None, 0, None, 0, 0, None))
-        og_current_page = current_page
-        if data == 'nyaa_back':
-            current_page -= 1
-        elif data == 'nyaa_next':
-            current_page += 1
-        if current_page < 1:
-            current_page = 1
-        elif current_page > pages:
-            current_page = pages
-        ttl_ended = (time.time() - ttl) > 3600
-        if ttl_ended:
-            text = getattr(message.text, 'html', 'Search expired')
-        else:
-            if callback_query.from_user.id != user_id:
-                await callback_query.answer('...no', cache_time=3600)
-                return
-            text, pages, ttl = await return_search(query, current_page, sukebei)
-        buttons = [InlineKeyboardButton(f'Prev', 'nyaa_back'), InlineKeyboardButton(f'{current_page}/{pages}', 'nyaa_nop'), InlineKeyboardButton(f'Next', 'nyaa_next')]
-        if ttl_ended:
-            buttons = [InlineKeyboardButton('Search Expired', 'nyaa_nop')]
-        else:
-            if current_page == 1:
-                buttons.pop(0)
-            if current_page == pages:
-                buttons.pop()
-        if ttl_ended or current_page != og_current_page:
-            await callback_query.edit_message_text(text, reply_markup=InlineKeyboardMarkup([
-                buttons
-            ]))
-        message_info[message_identifier] = user_id, ttl, query, current_page, pages, sukebei
-        if ttl_ended:
-            ignore.add(message_identifier)
-    await callback_query.answer()
 
 # Using upstream API based on: https://github.com/Ryuk-me/Torrents-Api
 # Implemented by https://github.com/jusidama18

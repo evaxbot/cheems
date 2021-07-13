0fdcb008dd6caa5646293c0b4b9219c599905b42
evaxbot <87263096+evaxbot@users.noreply.github.com>
2021-07-13 23:43:48 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,7 +13,7 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, speedtest
 
 
 @run_async
@@ -96,6 +96,8 @@ def bot_help(update, context):
 /{BotCommands.StatsCommand}: Show Stats of the machine the bot is hosted on
 
 /{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by owner of the bot)
+
+/{BotCommands.SpeedCommand}: Check Internet Speed of the Host
 
 /{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
 
